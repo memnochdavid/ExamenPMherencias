c45e8c2ff9360250f450228c379d92e5dc7a8bd1
memnochdavid <davduquediaz@gmail.com>
2024-12-05 20:18:04 +0100

    asdad

--- app/src/main/java/com/example/examenpmherencias/ExamenPMherencias.kt
+++ app/src/main/java/com/example/examenpmherencias/ExamenPMherencias.kt
@@ -1,6 +1,6 @@
 package com.example.examenpmherencias
 
-//var catalogo= mutableListOf<Videojuego>()//lista que tendría los juegos
+
 
 //Ejercicio 01
 class Videojuego(
@@ -47,13 +47,75 @@ fun plataformas(juegos:List<Videojuego>):MutableList<String>{
 
 
 //Ejercicio 05
-class Circunferencia(
-    var radio:Float
-    constructor(var centro)
-){
+open class Circunferencia(var radio:Float=0.0f){
+constructor(
+    x:Float,
+    y:Float,
+    centro:List<Float>
+):this()
+
+    fun longitud():Float{
+        return (2*Math.PI*radio).toFloat()
+    }
+
+    override fun toString(): String {
+        return "Radio: $radio, Longitud: ${longitud()}"
+    }
 }
+
+//Ejercicio 08
+class Circulo(radio: Float, val color: String) : Circunferencia(radio) {
+    constructor(centro: List<Float>, x: Float, y: Float, color: String) : this(
+        calculaRadio(centro, x, y),
+        color
+    )
+    override fun toString(): String {
+        return "Radio: $radio, Area: ${area(radio)}, color: $color, Longitud: ${longitud()}"
+    }
+
+
+    fun area( radio: Float): Float {
+        return (3.14f * (radio*radio))
+    }
+    companion object {
+        fun calculaRadio(centro: List<Float>, x: Float, y: Float): Float {//internet!!!!!
+            val centerX = centro[0]
+            val centerY = centro[1]
+            return Math.sqrt(Math.pow((x - centerX).toDouble(), 2.0) + Math.pow((y-centerY).toDouble(), 2.0)).toFloat()
+        }
+    }
+}
+
+//Ejercicio 09
+class Equipo(
+    var nombre:String,
+    var pais:String
+)
+
+//Ejercicio 10
+var liga= mutableListOf<Equipo>(
+    Equipo("Arsenal","Inglaterra"),
+    Equipo("Aston Vila","Italia"),
+    Equipo("Atalanta","Italia"),
+    Equipo("Atlético de Madrid","España"),
+    Equipo("Barcelona","España"),
+    Equipo("Bayer Leverkusen","Alemania"),
+    Equipo("Benfica","Portugal"),
+    Equipo("Borussia Dortmund","Alemania"),
+    Equipo("Celtic","Escocia"),
+    Equipo("Dinamo Zagreb","Croacia"),
+    Equipo("Real Madrid","España"),
+    Equipo("Girona","España"),
+    Equipo("Liverpool","Inglaterra"),
+    Equipo("Milan","Italia"),
+    Equipo("Stuttgart","Alemania"),
+    Equipo("Paris Saint-Germain","Francia"),
+    Equipo("Juventus","Italia"),
+)
+
 
 
 fun main(){
+
 
 }
